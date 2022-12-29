a05fbdd6db4e6d1a0e81cb6020748a9e3d2547f9
Claire Etienne <claireetienne17@gmail.com>
2022-12-29 18:20:34 -0500

    Initial Commit

---
+++ SourceControlFromExisitng/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SourceControlFromExisitng
+//
+//  Created by Claire Etienne on 12/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SourceControlFromExisitng/SourceControlFromExisitngApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlFromExisitngApp.swift
+//  SourceControlFromExisitng
+//
+//  Created by Claire Etienne on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlFromExisitngApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
